3c7589a1b1cafaab4b3ce2cce750555f9f5c3510
SeckinKurtalan <seckinkurtalan02@gmail.com>
2023-01-29 16:02:25 +0300

    Initial Commit
    ow yeah

---
+++ src/test/java/com/learningbackend/learningbackend/LearningbackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.learningbackend.learningbackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LearningbackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
