6fea28714d1502ae7a50b89fd20f94222a64f74c
Marrivara <berkaydulgerbaki@gmail.com>
2023-01-29 16:07:43 +0300

    Version test

--- src/main/java/com/learningbackend/learningbackend/LearningbackendApplication.java
+++ src/main/java/com/learningbackend/learningbackend/LearningbackendApplication.java
@@ -7,6 +7,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class LearningbackendApplication {
 
 	public static void main(String[] args) {
+		// Olum efsane çalışıyor lan bu
 		SpringApplication.run(LearningbackendApplication.class, args);
 	}
 
